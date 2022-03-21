cad4c395f1597f52df713dbc835e88c3eba34bde
moogieon <vzlzydn5007@gmail.com>
2022-03-21 16:07:39 +0900

    깃허브,포토폴리오 링크 추가

--- src/components/Main/Introduction.tsx
+++ src/components/Main/Introduction.tsx
@@ -10,7 +10,7 @@ const Introduction: FC = function () {
         backgroundImage: 'linear-gradient(60deg, #29323c 0%, #485563 100%)',
       }}
     >
-      <div className="flex justify-start container mx-auto">
+      <div className="flex justify-between container mx-auto">
         <div>
           <ProfileImage />
           <div className="mt-10 text-white">
@@ -18,6 +18,10 @@ const Introduction: FC = function () {
             <div className="text-2xl">I'm Junior Frontend Developer </div>
             <TypingText />
           </div>
+        </div>
+        <div>
+          <div>GIT-HUB</div>
+          <div>PORTFOLIO</div>
         </div>
       </div>
     </header>
