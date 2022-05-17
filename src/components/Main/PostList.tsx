c33fd040c49e1ff6af9913e09b9a1168f33b4d4a
moogieon <vzlzydn5007@gmail.com>
2022-05-17 09:03:02 +0900

    커텀훅스(무한스크롤) 추가-2

--- src/components/Main/PostList.tsx
+++ src/components/Main/PostList.tsx
@@ -1,6 +1,9 @@
 import React, { useMemo } from 'react'
 import PostItem from './PostItem'
 import { PostListItemType } from 'types/PostItem.types'
+import useInfiniteScroll, {
+  useInfiniteScrollType,
+} from 'hooks/useInfiniteScroll'
 
 type PostListProps = {
   posts: PostListItemType[]
@@ -25,22 +28,22 @@ const PostList: React.FC<PostListProps> = function ({
       ),
     [selectedCategory],
   )
+  const { containerRef, postList }: useInfiniteScrollType = useInfiniteScroll(
+    selectedCategory,
+    posts,
+  )
   return (
-    <>
-      <div className="mx-auto container">
-        <div className="grid grid-cols-[1fr,1fr] gap-[2%] pt-[50px] md:pb-[100px] md:grid-cols-1 md:py-[50px] md:px-[10%]">
-          {postListData.map(
-            ({ node: { id, frontmatter } }: PostListItemType) => (
-              <PostItem
-                {...frontmatter}
-                link="https://www.google.co.kr/"
-                key={id}
-              />
-            ),
-          )}
-        </div>
+    <div className="mx-auto container" ref={containerRef}>
+      <div className="grid grid-cols-[1fr,1fr] gap-[2%] pt-[50px] md:pb-[100px] md:grid-cols-1 md:py-[50px] md:px-[10%]">
+        {postListData.map(({ node: { id, frontmatter } }: PostListItemType) => (
+          <PostItem
+            {...frontmatter}
+            link="https://www.google.co.kr/"
+            key={id}
+          />
+        ))}
       </div>
-    </>
+    </div>
   )
 }
 
